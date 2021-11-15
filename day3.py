17c38f5383594d6a4e3356c0c27aab083e0f2bc5
Raghav <codingkapur@gmail.com>
2021-11-15 19:13:07 +0530

    end of day 3

---
+++ day3.py
@@ -0,0 +1,53 @@
+# water_level = int(input("What's the water level?\n"))
+# if water_level > 50:
+#     print("Time to drain the water")
+# elif water_level < 50:
+#     print("Keep it flowing")  
+# else:
+#     print("Close the tap and and jump in")
+
+# MODULO with ODD/EVEN
+# num = int(input("Enter a number to check if it's odd or even.\n"))
+
+# if(num==0):
+#     print("Zero, Bro")
+# elif num%2 ==0:
+#     print("Even, Bro")
+# else:
+#     print("Odd, Bro")
+
+# BMI 2.0
+# height = float(input("What is your height in meters? \n"))
+# weight = float(input("What is your weight in kgs? \n"))
+
+# bmi = round(weight/height**2,2)
+
+# if(bmi<18.5):
+#     print(f"Your bmi is {bmi} and you are underweight")
+# elif bmi<25:
+#     print(f"Your bmi is {bmi} and you are of normal weight")
+# elif bmi<30:
+#     print(f"Your bmi is {bmi} and you are overweight")
+# elif bmi<35:
+#     print(f"Your bmi is {bmi} and buddy you are obese")
+# else:
+#     print(f"Your bmi is {bmi} and you are pretty much screwed")
+
+
+# LEAP YEAR?
+# year = int(input("Enter a year buddy\n"))
+
+# if(year%100==0):
+#     if(year%400==0):
+#         print("This is a leap year, son")
+#     else:
+#         print("Not a leap year son")
+# elif year%4==0:
+#     print("This is a leap year, son")
+# else:
+#     print("Not even close to a leap year, you dimwit")
+
+# logical operators
+# and is and
+# or is or
+# not condition is like !true.
